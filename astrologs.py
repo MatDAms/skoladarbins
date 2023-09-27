41b01580e3cef67b66a6051fe813f4a8a830c5de
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2023-09-27 09:19:56 +0300

    izmaiņas

---
+++ astrologs.py
@@ -0,0 +1,6 @@
+import math
+import random
+def nedelas_horoskopi(apgalvojums, divdabis, rīkojums):
+ apgalvojums = ["aries","taurus","gemini","cancer"]
+ divdabis = ["smags", "liels","mazs"]
+ rīkojums = ["darba spējigs","slinkais"]
