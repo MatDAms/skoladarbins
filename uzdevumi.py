3e54c9b012ae6099c7addce7f87583f7a5b49c02
MatDAms <145021121+MatDAms@users.noreply.github.com>
2023-09-27 08:09:40 +0300

    Add files via upload

---
+++ uzdevumi.py
@@ -0,0 +1,7 @@
+def linulejas(daudzums, cena):
+    cena = float(input("kada ir cena linolejam"))
+    daudzums = float(input("kads ir linoleju daudzums"))
+    summa= daudzums * cena 
+    return summa
+print(linulejas)
+    
