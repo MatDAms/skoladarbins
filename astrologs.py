94808132a13e415938d80bf34a800235dfe60b28
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2023-09-28 14:42:50 +0300

    sanak

--- astrologs.py
+++ astrologs.py
@@ -1,6 +1,12 @@
 import math
 import random
-def nedelas_horoskopi(apgalvojums, divdabis, rīkojums):
- apgalvojums = ["aries","taurus","gemini","cancer"]
- divdabis = ["smags", "liels","mazs"]
- rīkojums = ["darba spējigs","slinkais"]
+list=["aries","taurus","gemina",'cancer']
+apgalvojumi=["Like their fellow fire signs, Leo and Sagittarius, Aries is a passionate, motivated, and confident leader","Tauruses have a regal quality about them. They are graceful and diligent laborers.","veidots konferenču krēsls “Gemina”. Moderns, praktisks un ērts  šie ir vārdi, kas raksturo “Gemina” krēslu"  ]
+random.shuffle(list)
+random.shuffle(apgalvojumi)
+for x in list:
+    print(x)
+for i in apgalvojumi:
+    print(i)
+
+print(x,i)
