323d3e84dbad14be946d1ea418acd7b92444957a
Andrejs A <145021121+MatDAms@users.noreply.github.com>
2023-10-05 15:27:58 +0300

    ieskaite

--- udzevums p1.py
+++ udzevums p1.py
@@ -3,7 +3,7 @@ x = str(input("uzrakstiet vardu un uzvardu"))
 c =int(input("cik stradajat mēnesi"))#pasaka pamat informaciju#
 def darbs(stundas):
     return c*stundas
-print(darbs(random.randrange(4,5)))
+print(darbs(random.randrange(4)))
 if c<40:
     print("ši ir mēneša maksa",darbs)
 else:
