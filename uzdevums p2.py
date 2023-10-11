34d4bcdec7704fd7ab9d5f53f7292ac040c51391
Andrejs A <145021121+MatDAms@users.noreply.github.com>
2023-10-11 08:13:52 +0300

    god

---
+++ uzdevums p2.py
@@ -0,0 +1,14 @@
+import random
+import math
+garums = int(input("garuma m"))
+platums = int(input("platums m")) # izmeri paralēskaldnim#
+biezums = int(input("biezums m"))
+x = ("tilpums",garums * platums * biezums)
+print (x)
+
+pirma= (145)
+otra = (125)
+if pirma > otra:
+    print("tik daudz jamaksa",pirma*garums*platums*biezums)
+else:
+    print("tik daudz jamaksa",otra*x)
