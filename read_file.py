0f36780c70094cf34ad3f99129fdc3b2ed1263ee
Andrejs A <145021121+MatDAms@users.noreply.github.com>
2023-11-02 13:46:19 +0200

    stuf

--- read_file.py
+++ read_file.py
@@ -1,2 +1,19 @@
-f = open("mansteksts.txt", "r")
+#f = open("mansteksts.txt", "r")
+#print(f.read())#
+#f = open("demofile.txt", "r")
+#for x in f:
+#  print(x)
+
+
+#f = open("mansteksts.txt", "r")
+#print(f.readline())
+#f.close()
+
+
+f = open("mansteksts1.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("mansteksts1.txt", "r")
 print(f.read())
