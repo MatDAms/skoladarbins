04559023542af787c02069e6dbfa85c35120d548
Andrejs A <145021121+MatDAms@users.noreply.github.com>
2023-12-20 08:53:08 +0200

    turtle_paint

---
+++ turtl_move.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.fillcolor("Black")
+turtle.begin_fill()
+turtle.pencolor("Red")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.end_fill()
+turtle.done()
